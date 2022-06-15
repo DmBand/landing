8dadeaff37e261d39c3a83a64831597e2707f58f
DmBand <d.bandysik@gmail.com>
2022-06-15 16:37:33 +0300

    back call

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,14 +4,16 @@ const catalogBtn = document.querySelectorAll('.catalog-btn');
 const catalogButtons = document.querySelector('.catalog-buttons');
 const catalog = document.querySelector('.catalog');
 
-function activeBtn(event) {
-    const product = event.target.dataset.product;
-    catalogBtn.forEach((btn) => btn.classList.remove('btn-active'))    
-    event.target.classList.add('btn-active');
-    const currentDiv = document.querySelector('.active-product');
-    currentDiv.classList.remove('active-product');
-    const div = document.querySelector(`.one-product-container-${product}`);
-    div.classList.add('active-product')
+function activeBtn(event) {       
+    if (event.target.tagName == 'BUTTON') {
+        const product = event.target.dataset.product; 
+        catalogBtn.forEach((btn) => btn.classList.remove('btn-active'))
+        event.target.classList.add('btn-active');
+        const currentDiv = document.querySelector('.active-product');
+        currentDiv.classList.remove('active-product');
+        const div = document.querySelector(`.one-product-container-${product}`);
+        div.classList.add('active-product')
+    }   
 }
 
 catalogButtons.addEventListener('click', activeBtn)
