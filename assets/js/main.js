60edbd7f771e79112390a9528e6dab21cd95c928
DmBand <d.bandysik@gmail.com>
2022-06-18 15:39:05 +0300

    new delivery page and some changes main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const logo = document.querySelector('.main-logo');
     const text = document.querySelector('.main-text');
+    const link = document.querySelector('.about-link');
     logo.classList.add('visible');
-    text.classList.add('visible');    
+    text.classList.add('visible');  
+    link.classList.add('visible');    
 })
