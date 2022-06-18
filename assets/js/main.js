2e22887e7ac7cdbc83f14a52ec795a999fee1437
DmBand <d.bandysik@gmail.com>
2022-06-18 14:58:15 +0300

    main page

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', function () {
+    const logo = document.querySelector('.main-logo');
+    const text = document.querySelector('.main-text');
+    logo.classList.add('visible');
+    text.classList.add('visible');    
+})
