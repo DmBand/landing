4d0b83c898e592f679c3d8e9e6d888f977d3e25c
DmBand <d.bandysik@gmail.com>
2022-06-23 12:20:48 +0300

    hosting db

--- landing/settings.py
+++ landing/settings.py
@@ -76,10 +76,25 @@ WSGI_APPLICATION = 'landing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        },
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': env.int('DB_PORT'),
     }
 }
 
